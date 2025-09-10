82e441c6141be7477a722400bcd967f72320f8a8
Tiep Nguyen <tnguyen668@ucmerced.edu>
2025-09-10 11:37:44 -0700

    Fixed typo

--- autotuning/src/HPLWrapper/HPLConfig.py
+++ autotuning/src/HPLWrapper/HPLConfig.py
@@ -47,7 +47,7 @@ class HPLConfig:
     Threshold: float = 16.0 # line 13
     PFact_Array: List[PFactEnum] = field(default_factory=lambda: [PFactEnum.Left, PFactEnum.Crout, PFactEnum.Right]) # line 14-15
     NBMin_Array: List[int] # line 16-17
-    NDIV_Array: List[int] =  # line 18-19
+    NDIV_Array: List[int]  # line 18-19
     RFact_Array: List[RFactEnum] = field(default_factory=lambda: [RFactEnum.Left, RFactEnum.Crout, RFactEnum.Right])  # (0=left, 1=Crout, 2=Right) line 20-21
     BCAST_Array: List[BCastEnum] = field(default_factory=lambda: [BCastEnum.OneRing])  # (0=1rg,1=1rM,2=2rg,3=2rM,4=Lng,5=LnM) line 22-23
     Depth_Array: List[int] = field(default_factory=lambda: [0]) # line 24-25
